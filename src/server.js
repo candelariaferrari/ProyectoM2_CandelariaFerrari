09202fa88d5dceb009472c374bdd834e36a5a81d
Candelaria <ferrari.candelaria@gmail.com>
2026-06-16 15:23:08 -0300

    fix: correción para railway

--- src/server.js
+++ src/server.js
@@ -7,24 +7,27 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./docs/openAPI.yaml");
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middlewares globales
 app.use(express.json());
 
-
 // ruta raíz
+app.get("/", (req, res) => {
+  res.json({
+    message: "MiniBlog API corriendo",
+    docs: "/api-docs",
+  });
+});
+
 app.use("/api/authors", authorsRouter);
 
 app.use("/api/posts", postsRouter);
 
 app.use(errorHandler);
 
-
 app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 
 module.exports = app;
